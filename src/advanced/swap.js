aa2698d71a743b53a72b8dd0ac0a0d00564c0ab5
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-20 10:25:13 +0200

    All assignments completed.

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,7 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+b += a
+a = b - a
+b = b - a
 module.exports = { a, b }
